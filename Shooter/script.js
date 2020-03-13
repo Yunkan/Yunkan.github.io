5ede08abf8866a110e1a0e4a90feca5b5d6f95fc
Yunkan <unknsoul@mail.ru>
2020-03-13 16:58:39 +0300

    update1

--- Shooter/script.js
+++ Shooter/script.js
@@ -479,7 +479,7 @@ function positionHandler(e)
 	}
 	else if (e.targetTouches)
 	{
-		mouse.x = e.targetTouches[0].offsetX;
+		mouse.x = e.targetTouches[0].clientX;
 		mouse.y = e.targetTouches[0].clientY;
 		sc.innerHTML = e.targetTouches[0].offsetX;
 		e.preventDefault();
