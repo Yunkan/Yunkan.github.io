d3e748a5ee4d8fc89171fd1b0c0ff20305458644
Yunkan <unknsoul@mail.ru>
2021-05-12 17:45:55 +0300

    Life update

--- Life/script.js
+++ Life/script.js
@@ -1,149 +1,227 @@
-var ctx = cnv.getContext('2d');
+const cnv = document.getElementById('cnv');
+const ctx = cnv.getContext('2d');
 
 cnv.width = 700;
 cnv.height = 700;
 
-function getRandom(min, max)
-{
-	return Math.floor(min + Math.random() * (max + 1 - min));
+const cols = document.getElementById('cols');
+const rows = document.getElementById('rows');
+const size = document.getElementById('size');
+const startBtn = document.getElementById('startBtn');
+const clearBtn = document.getElementById('clearBtn');
+const randomBtn = document.getElementById('randomBtn');
+
+let startLife = false;
+let colAmount = 70;
+let rowAmount = 70;
+let cellSize = 10;
+
+let population = [];
+let population2 = [];
+
+const mouse = {
+	x: 0,
+	y: 0,
+	draw: false
 }
 
-var population = [],
-	population2 = [],
-	life = false,
-	cycle = 0;
-
-cnv.onclick = function(e)
-{
-	var x = e.offsetX;
-	var y = e.offsetY;
-
-	x = Math.floor(x / 10);
-	y = Math.floor(y / 10);
-
-	for(var i = 0; i < population.length; i++)
-	{
-		for(var j = 0; j < population[i].length; j++)
-		{
-			if(i == x && j == y && !life)
-			{
-				population[i][j] == 0 ? population[i][j] = 1 : population[i][j] = 0;
-			}
-		}
-	}
+function getRandom(min, max) {
+	return ~~(min + Math.random() * (max + 1 - min));
 }
 
-function clearGenerate()
-{
-	for(var i = 0; i < 70; i++)
-	{
+function clearGenerate() {
+	population = [];
+	for(var i = 0; i < colAmount; i++) {
 		population[i] = [];
-		for(var j = 0; j < 70; j++)
-		{
+		for(var j = 0; j < rowAmount; j++) {
 			population[i][j] = 0;
 		}
 	}
 }
 
-function randomGenerate()
-{
-	if(!life)
-	{
+function randomGenerate() {
+	if(!startLife) {
 		clearGenerate();
-		for(var i = 0; i < 70; i++)
-		{
+		for(var i = 0; i < colAmount; i++) {
 			population[i] = [];
-			for(var j = 0; j < 70; j++)
-			{
+			for(var j = 0; j < rowAmount; j++) {
 				population[i][j] = getRandom(0, 1);
 			}
 		}
 	}
 }
 
-function drawField()
-{
-	for(var x = 0; x < 70; x++)
-	{
-		for(var y = 0; y < 70; y++)
-		{
-			ctx.strokeStyle = "#aaa";
-			ctx.strokeRect(x * 10, y * 10, 10, 10);
+function drawField() {
+	cnv.width = colAmount * cellSize;
+	cnv.height = rowAmount * cellSize;
+	ctx.fillStyle = '#ddd';
+	ctx.fillRect(0, 0, cnv.width, cnv.height);
+	for(var x = 0; x < colAmount; x++) {
+		for(var y = 0; y < rowAmount; y++) {
+			drawCell(x, y);
 		}
 	}
 }
 
-function draw()
-{
-	for(var i = 0; i < population.length; i++)
-	{
-		for(var j = 0; j < population[i].length; j++)
-		{
-			if(population[i][j] == 1)
-			{
-				ctx.fillRect(i * 10, j * 10, 10, 10);
-			}
-		}
+function drawCell(i, j) {
+	if(population[i][j] == 1) {
+		ctx.fillStyle = '#000';	
+		ctx.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
+	} else {
+		ctx.fillStyle = '#ddd';	
+		ctx.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
+		ctx.strokeStyle = '#aaa';
+		ctx.strokeRect(i * cellSize, j * cellSize, cellSize, cellSize);
 	}
-
-	if(life)
-		step();
 }
 
-function step()
-{
+function step() {
 	population2 = [];
-	for(var i = 0; i < population.length; i++)
-	{
+	for(let i = 0; i < population.length; i++) {
 		population2[i] = [];
-		for(var j = 0; j < population[i].length; j++)
-		{
-			var neighbors = 0;
+		for(let j = 0; j < population[i].length; j++) {
+			let neighbors = 0;
 
-			if(population[fpm(i) - 1][j] == 1) neighbors++;
-			if(population[i][fpp(j) + 1] == 1) neighbors++;
-			if(population[fpp(i) + 1][j] == 1) neighbors++;
-			if(population[i][fpm(j) - 1] == 1) neighbors++;
-			if(population[fpm(i) - 1][fpp(j) + 1] == 1) neighbors++;
-			if(population[fpp(i) + 1][fpp(j) + 1] == 1) neighbors++;
-			if(population[fpp(i) + 1][fpm(j) - 1] == 1) neighbors++;
-			if(population[fpm(i) - 1][fpm(j) - 1] == 1) neighbors++;
+			if(population[fpm(i, 'col') - 1][j] == 1) neighbors++;
+			if(population[i][fpp(j, 'row') + 1] == 1) neighbors++;
+			if(population[fpp(i, 'col') + 1][j] == 1) neighbors++;
+			if(population[i][fpm(j, 'row') - 1] == 1) neighbors++;
+			if(population[fpm(i, 'col') - 1][fpp(j, 'row') + 1] == 1) neighbors++;
+			if(population[fpp(i, 'col') + 1][fpp(j, 'row') + 1] == 1) neighbors++;
+			if(population[fpp(i, 'col') + 1][fpm(j, 'row') - 1] == 1) neighbors++;
+			if(population[fpm(i, 'col') - 1][fpm(j, 'row') - 1] == 1) neighbors++;
 
 			if(population[i][j] == 0 && neighbors == 3) population2[i][j] = 1;
 			(population[i][j] == 1 && neighbors == 2 || neighbors == 3) ? population2[i][j] = 1 : population2[i][j] = 0;
 		}
 	}
-
 	population = population2;
-	cycle++;
-	document.querySelector(".cycle").innerHTML = "Цикл: " + cycle;
 }
 
-function fpm(i)
-{
-	if(i == 0) return 70;
+function fpm(i, line) {
+	let num = 0;
+	if(line === 'col')
+		num = colAmount;
+	else
+		num = rowAmount;
+
+	if(i === 0) return num;
 	else return i;
 }
 
-function fpp(i)
-{
-	if(i == 69) return -1;
+function fpp(i, line) {
+	let num = 0;
+	if(line === 'col')
+		num = colAmount;
+	else
+		num = rowAmount;
+
+	if(i === num - 1) return -1;
 	else return i;
 }
+
+function animate() {
+	if(startLife) {
+		ctx.clearRect(0, 0, cnv.width, cnv.height);
+
+		drawField();
+		step();
+
+		requestAnimationFrame(animate);
+	}
+}
+
+cols.addEventListener('change', () => {
+	document.getElementById('colValue').innerHTML = cols.value;
+	colAmount = +cols.value;
+	clearGenerate();
+	drawField();
+});
+
+rows.addEventListener('change', () => {
+	document.getElementById('rowValue').innerHTML = rows.value;
+	rowAmount = +rows.value;
+	clearGenerate();
+	drawField();
+});
+
+size.addEventListener('change', () => {
+	document.getElementById('sizeValue').innerHTML = size.value;
+	cellSize = +size.value;
+	clearGenerate();
+	drawField();
+});
+
+startBtn.addEventListener('click', () => {
+	if(startBtn.innerHTML === 'Старт') {
+		startBtn.innerHTML = 'Стоп';
+		startLife = true;
+		cols.disabled = true;
+		rows.disabled = true;
+		size.disabled = true;
+		clearBtn.disabled = true;
+		randomBtn.disabled = true;
+		animate();
+	} else {
+		startBtn.innerHTML = 'Старт';
+		startLife = false;
+		cols.disabled = false;
+		rows.disabled = false;
+		size.disabled = false;
+		clearBtn.disabled = false;
+		randomBtn.disabled = false;
+	}
+});
+
+clearBtn.addEventListener('click', () => {
+	clearGenerate();
+	drawField();
+});
+
+randomBtn.addEventListener('click', () => {
+	randomGenerate();
+	drawField();
+});
+
+function cellPrimaryInteract(e) {
+	if(!startLife) {
+		if(e.which == 1) {
+			mouse.draw = true;
+			population[mouse.x][mouse.y] = 1;
+			drawCell(mouse.x, mouse.y);
+		} else if(e.which == 3) {
+			mouse.clear = true;			
+			population[mouse.x][mouse.y] = 0;
+			drawCell(mouse.x, mouse.y);
+		}
+	}
+}
+
+cnv.addEventListener('mousedown', cellPrimaryInteract);
+cnv.addEventListener('contextmenu', (e) => e.preventDefault());
+
+cnv.addEventListener('mouseup', (e) => {
+	mouse.draw = false;
+	mouse.clear = false;
+});
+
+cnv.addEventListener('mousemove', (e) => {
+	mouse.x = ~~(e.offsetX / cellSize);
+	mouse.y = ~~(e.offsetY / cellSize);
+	for(let i = 0; i < population.length; i++) {
+		for(let j = 0; j < population[i].length; j++) {
+			if(i == mouse.x && j == mouse.y && !startLife) {
+				if(mouse.draw) {
+					population[i][j] = 1;
+					drawCell(i, j);
+				} else if(mouse.clear) {
+					population[i][j] = 0;
+					drawCell(i, j);
+				}
+			}
+		}
+	}
+});
 
 clearGenerate();
-
-setInterval(function()
-{
-	ctx.clearRect(0, 0, cnv.width, cnv.height);
-	drawField();
-	draw();
-}, 100)
-
-function stopLife()
-{
-	life = false;
-	cycle = 0;
-	document.querySelector(".cycle").innerHTML = "Цикл: " + cycle;
-	clearGenerate();
-}
+drawField();
